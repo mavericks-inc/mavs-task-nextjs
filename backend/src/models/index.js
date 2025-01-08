d9f985f6e6320d1e6bbc6b664a6c2a16fccc20b4
YumaEndo921 <129018764+YumaEndo921@users.noreply.github.com>
2025-01-08 22:23:54 +0900

    update :疎通可能状態

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,17 +1,29 @@
 'use strict';
+import dotenv from 'dotenv';
 import process from 'process';
 import Sequelize from 'sequelize';
-import config from '../config/config.json' assert { type: 'json' };
+import config from '../config.js';
 import Users from './users.js';
 import Articles from './articles.js';
+// .envファイルから環境変数をロード
+dotenv.config();
+
+// 環境変数からデータベース接続URLを取得
+const dbUrl = process.env.DATABASE_URL;
+
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+sequelize = new Sequelize(dbUrl, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true, // SSLを必須に設定
+      rejectUnauthorized: false, // セルフサイン証明書を使用している場合、これをfalseにする必要があります
+    },
+  },
+  logging: false, // ログの表示を無効にする（任意）
+});
 
 db['Users'] = Users(sequelize, Sequelize.DataTypes);
 db['Articles'] = Articles(sequelize, Sequelize.DataTypes);
